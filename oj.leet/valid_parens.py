5b5a5f572291e4d702d36cf44be36a28a0927276
Brant Strand <bstrand@gmail.com>
2015-02-23 00:40:01 -0800

    Valid Parentheses initial 40ms soln

---
+++ oj.leet/valid_parens.py
@@ -0,0 +1,46 @@
+# https://oj.leetcode.com/problems/valid-parentheses/
+
+import unittest
+
+class Solution:
+    # @return a boolean
+    openers = {'{':'}','[':']', '(':')'}
+    closers = {v: k for k, v in openers.items()}
+    
+    def isValid(self, s):
+        parens = list(s)
+        next_close = []
+        for tok in parens:
+            #print "t: %s" % t
+            if tok in self.openers.keys():
+                next_close.append(self.openers[tok])
+            elif tok in self.closers.keys():
+                if not next_close or next_close.pop() != tok:
+                    #print "INVALID"
+                    return False
+        #print "next_close len = %d --> %r" % (len(next_close), len(next_close) == 0)
+        return len(next_close) == 0
+
+class TestSolution(unittest.TestCase):
+    def test_one_of_each(self):
+        sln = Solution()
+        self.assertEqual(True, sln.isValid("{}()[]"))
+ 
+    def test_one_of_each_nested(self):
+        sln = Solution()
+        self.assertEqual(True, sln.isValid("{([])}"))
+ 
+    def test_two_interleaved(self):
+        sln = Solution()
+        self.assertEqual(False, sln.isValid("([)]"))
+ 
+    def test_two_left_open(self):
+        sln = Solution()
+        self.assertEqual(False, sln.isValid("(["))
+ 
+    def test_two_close_only(self):
+        sln = Solution()
+        self.assertEqual(False, sln.isValid("])"))
+ 
+if __name__ == '__main__':
+    unittest.main()
