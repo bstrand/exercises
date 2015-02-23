9dfd38ec6f3f0bb7d00377dc2494d822f1a5baeb
Brant Strand <bstrand@gmail.com>
2015-02-23 01:10:28 -0800

    Valid Parentheses refactor more C-like, 40ms

--- oj.leet/valid_parens.py
+++ oj.leet/valid_parens.py
@@ -4,21 +4,38 @@ import unittest
 
 class Solution:
     # @return a boolean
-    openers = {'{':'}','[':']', '(':')'}
-    closers = {v: k for k, v in openers.items()}
+    # openers = {'{':'}','[':']', '(':')'}
+    # closers = {v: k for k, v in openers.items()}
     
+    # def isValid1(self, s):
+    #     next_close = []
+    #     for c in s:
+    #         #print "c: %c" % c
+    #         if c in self.openers.keys():
+    #             next_close.append(self.openers[c])
+    #         elif c in self.closers.keys():
+    #             if not next_close or next_close.pop() != c:
+    #                 #print "INVALID"
+    #                 return False
+    #     #print "next_close len = %d --> %r" % (len(next_close), len(next_close) == 0)
+    #     return not next_close
+
     def isValid(self, s):
-        next_close = []
+        paren_stack = []
         for c in s:
-            #print "c: %c" % c
-            if c in self.openers.keys():
-                next_close.append(self.openers[c])
-            elif c in self.closers.keys():
-                if not next_close or next_close.pop() != c:
-                    #print "INVALID"
-                    return False
-        #print "next_close len = %d --> %r" % (len(next_close), len(next_close) == 0)
-        return not next_close
+            #print "<-- %c" % c
+            if not paren_stack:
+                paren_stack.append(c)
+            else:
+                if (ord(c) == 1 + ord(paren_stack[-1]) or (ord(c) == 2 + ord(paren_stack[-1]))):
+                    paren_stack.pop()
+                else:
+                    paren_stack.append(c)
+            #print paren_stack
+        if not paren_stack:
+            return True
+        return False
+
 
 class TestSolution(unittest.TestCase):
     def test_one_of_each(self):
@@ -40,6 +57,10 @@ class TestSolution(unittest.TestCase):
     def test_two_close_only(self):
         sln = Solution()
         self.assertEqual(False, sln.isValid("])"))
+ 
+    def test_extra_open(self):
+        sln = Solution()
+        self.assertEqual(False, sln.isValid("([)"))
  
 if __name__ == '__main__':
     unittest.main()
