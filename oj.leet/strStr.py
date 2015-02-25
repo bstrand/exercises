be6675e96859181c4800270357bef4e2002206ce
Brant Strand <bstrand@gmail.com>
2015-02-24 19:06:21 -0800

    strStr brute force

---
+++ oj.leet/strStr.py
@@ -0,0 +1,89 @@
+# https://oj.leetcode.com/problems/implement-strstr/
+
+import unittest
+
+class Solution:
+    # @param haystack, a string
+    # @param needle, a string
+    # @return an integer
+    def strStrSlow(self, haystack, needle):
+        for i in range(len(haystack)):
+            if self.starts_with(haystack[i:], needle):
+                return i
+        if len(haystack) == 0:
+            if len(needle) == 0:
+                return 0
+        return -1
+
+    def starts_with(self, full, prefix):
+        f_len = len(full)
+        p_len = len(prefix)
+        if f_len < p_len:
+            return False
+        print "Full: {}, Prefix: {}".format(full, prefix)
+        for i in range(p_len):
+            if full[i] != prefix[i]:
+                return False
+        return True
+
+    def strStr(self, haystack, needle):
+        h_len = len(haystack)
+        n_len = len(needle)
+        if not n_len:
+            return 0
+        if h_len < n_len:
+            return -1
+
+        print "Full: {} ({}), Prefix: {} ({})".format(haystack, h_len, needle, n_len)
+        for i in range(h_len):
+            for j in range(n_len):
+                print "h[{}]:n[{}] {}:{}".format(i+j, j, haystack[i+j], needle[j])                
+                if i + n_len > h_len :
+                    return -1 
+                if haystack[i+j] == needle[j]:
+                    if j == n_len-1:
+                        return i
+                    next
+                else:
+                    break
+        return -1
+
+class TestSolution(unittest.TestCase):
+    def test_01(self):
+        sln = Solution()
+        haystack = 'abcdeffoobar'
+        needle = 'foo'
+        self.assertEqual(haystack.find(needle), sln.strStr(haystack, needle))
+
+    def test_mississippi(self):
+        sln = Solution()
+        haystack = 'mississippi'
+        needle = 'issipi'
+        self.assertEqual(haystack.find(needle), sln.strStr(haystack, needle))
+
+    def test_aaa(self):
+        sln = Solution()
+        haystack = 'aaa'
+        needle = 'aaaa'
+        self.assertEqual(haystack.find(needle), sln.strStr(haystack, needle))
+
+    def test_a_plus_empty(self):
+        sln = Solution()
+        haystack = 'a'
+        needle = ''
+        self.assertEqual(haystack.find(needle), sln.strStr(haystack, needle))
+
+    def test_a_a(self):
+        sln = Solution()
+        haystack = 'a'
+        needle = 'a'
+        self.assertEqual(haystack.find(needle), sln.strStr(haystack, needle))
+
+    def test_empty(self):
+        sln = Solution()
+        haystack = ''
+        needle = ''
+        self.assertEqual(haystack.find(needle), sln.strStr(haystack, needle))
+
+if __name__ == '__main__':
+    unittest.main()
