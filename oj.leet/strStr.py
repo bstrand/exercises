c53a9504b7201168738bc3576228daa77b05f654
Brant Strand <bstrand@gmail.com>
2015-02-24 19:41:44 -0800

    strStr recursive, concise and fastest at 40ms

--- oj.leet/strStr.py
+++ oj.leet/strStr.py
@@ -6,7 +6,21 @@ class Solution:
     # @param haystack, a string
     # @param needle, a string
     # @return an integer
-    def strStrSlow(self, haystack, needle):
+    def strStr(self, haystack, needle, depth=0):
+        # print "{} in {} ? d={}".format(needle,haystack,depth)
+        if not len(needle):
+            return 0
+        if not len(haystack):
+            return -1
+        if len(haystack) < len(needle):
+            return -1
+        if haystack[:len(needle)] == needle:
+            return depth
+        else:
+            return self.strStr(haystack[1:], needle, depth+1)
+
+
+    def strStrSlowWithHelperMethod(self, haystack, needle):
         for i in range(len(haystack)):
             if self.starts_with(haystack[i:], needle):
                 return i
@@ -20,13 +34,13 @@ class Solution:
         p_len = len(prefix)
         if f_len < p_len:
             return False
-        print "Full: {}, Prefix: {}".format(full, prefix)
+        # print "Full: {}, Prefix: {}".format(full, prefix)
         for i in range(p_len):
             if full[i] != prefix[i]:
                 return False
         return True
 
-    def strStr(self, haystack, needle):
+    def strStrBrute(self, haystack, needle):
         h_len = len(haystack)
         n_len = len(needle)
         if not n_len:
@@ -34,10 +48,10 @@ class Solution:
         if h_len < n_len:
             return -1
 
-        print "Full: {} ({}), Prefix: {} ({})".format(haystack, h_len, needle, n_len)
+        # print "Full: {} ({}), Prefix: {} ({})".format(haystack, h_len, needle, n_len)
         for i in range(h_len):
             for j in range(n_len):
-                print "h[{}]:n[{}] {}:{}".format(i+j, j, haystack[i+j], needle[j])                
+                # print "h[{}]:n[{}] {}:{}".format(i+j, j, haystack[i+j], needle[j])                
                 if i + n_len > h_len :
                     return -1 
                 if haystack[i+j] == needle[j]:
@@ -51,7 +65,7 @@ class Solution:
 class TestSolution(unittest.TestCase):
     def test_01(self):
         sln = Solution()
-        haystack = 'abcdeffoobar'
+        haystack = 'abcffoob'
         needle = 'foo'
         self.assertEqual(haystack.find(needle), sln.strStr(haystack, needle))
 
@@ -67,10 +81,16 @@ class TestSolution(unittest.TestCase):
         needle = 'aaaa'
         self.assertEqual(haystack.find(needle), sln.strStr(haystack, needle))
 
-    def test_a_plus_empty(self):
+    def test_a_empty(self):
         sln = Solution()
         haystack = 'a'
         needle = ''
+        self.assertEqual(haystack.find(needle), sln.strStr(haystack, needle))
+
+    def test_empty_a(self):
+        sln = Solution()
+        haystack = ''
+        needle = 'a'
         self.assertEqual(haystack.find(needle), sln.strStr(haystack, needle))
 
     def test_a_a(self):
@@ -79,7 +99,7 @@ class TestSolution(unittest.TestCase):
         needle = 'a'
         self.assertEqual(haystack.find(needle), sln.strStr(haystack, needle))
 
-    def test_empty(self):
+    def test_empty_empty(self):
         sln = Solution()
         haystack = ''
         needle = ''
